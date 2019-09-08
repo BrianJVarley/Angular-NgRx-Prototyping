66d0727790a78eaff84592c333f54e7fa5b13e4b
BrianJVarley <brianvarley9@gmail.com>
2019-09-08 21:42:46 +1000

    strongly type the productList action creators, remove magic string actions

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { ProductService } from '../product.service';
 import { Store, select } from '@ngrx/store';
 
 import * as fromProduct from '../state/product.reducer';
+import * as productActions from '../state/product.actions';
 
 @Component({
 	selector: 'pm-product-list',
@@ -50,10 +51,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	checkChanged(value: boolean): void {
-		this.store.dispatch({
-			type: 'TOGGLE_PRODUCT_CODE',
-			payload: value
-		});
+		this.store.dispatch(new productActions.ToggleProductCode(true));
 	}
 
 	newProduct(): void {
--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,6 +1,7 @@
 import { Product } from '../product';
 import * as fromRoot from '../../state/app.state';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ProductActions, ProductActionTypes } from './product.actions';
 
 // extend Root State to assign lazy loaded
 // ProductState when component loaded
@@ -52,16 +53,41 @@ export const getProducts = createSelector(
  * @param state 
  * @param action 
  */
-export function reducer(state = initialState, action): ProductState {
-	switch (action.type) {
-		case 'TOGGLE_PRODUCT_CODE':
-			return {
-                ...state,
-                showProductCode: action.payload
-            };
+export function reducer(state = initialState, action: ProductActions): ProductState {
 
-		default:
-			return state;
+	switch (action.type) {
+	  case ProductActionTypes.ToggleProductCode:
+		return {
+		  ...state,
+		  showProductCode: action.payload
+		};
+  
+	  case ProductActionTypes.SetCurrentProduct:
+		return {
+		  ...state,
+		  currentProduct: { ...action.payload }
+		};
+  
+	  case ProductActionTypes.ClearCurrentProduct:
+		return {
+		  ...state,
+		  currentProduct: null
+		};
+  
+	  case ProductActionTypes.InitializeCurrentProduct:
+		return {
+		  ...state,
+		  currentProduct: {
+			id: 0,
+			productName: '',
+			productCode: 'New',
+			description: '',
+			starRating: 0
+		  }
+		};
+  
+	  default:
+		return state;
 	}
-}
+  }
 
