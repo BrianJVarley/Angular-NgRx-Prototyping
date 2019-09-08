b909908c7c27bfa1c37e8f4e7e593c4d4c23b3ce
BrianJVarley <brianvarley9@gmail.com>
2019-09-08 21:51:22 +1000

    refactor component to call actions instead of service, subscribe to store selectors

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -29,8 +29,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	constructor(private store: Store<fromProduct.State>, private productService: ProductService) { }
 
 	ngOnInit(): void {
-		this.sub = this.productService.selectedProductChanges$.subscribe(
-			selectedProduct => (this.selectedProduct = selectedProduct)
+		this.sub = this.store.pipe(select(fromProduct.getCurrentProduct)).subscribe(
+			currentProduct => this.selectedProduct = currentProduct
 		);
 
 		this.productService.getProducts().subscribe({
@@ -47,7 +47,6 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnDestroy(): void {
-		this.sub.unsubscribe();
 	}
 
 	checkChanged(value: boolean): void {
@@ -55,10 +54,10 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	newProduct(): void {
-		this.productService.changeSelectedProduct(this.productService.newProduct());
+		this.store.dispatch(new productActions.InitializeCurrentProduct());
 	}
 
 	productSelected(product: Product): void {
-		this.productService.changeSelectedProduct(product);
+		this.store.dispatch(new productActions.SetCurrentProduct(product));
 	}
 }
