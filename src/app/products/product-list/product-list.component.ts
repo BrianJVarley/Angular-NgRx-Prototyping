007e1de061d3769a1f203647480647ad9f1bdfeb
BrianJVarley <brianvarley9@gmail.com>
2019-09-08 21:43:50 +1000

    correct action invocation params

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -51,7 +51,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	checkChanged(value: boolean): void {
-		this.store.dispatch(new productActions.ToggleProductCode(true));
+		this.store.dispatch(new productActions.ToggleProductCode(value));
 	}
 
 	newProduct(): void {
